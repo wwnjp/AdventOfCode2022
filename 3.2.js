9421560affcbe7bcbc3994e981280bc47428c88d
Josh Drumm <wwnjp@users.noreply.github.com>
2022-12-03 21:50:19 -0500

    Add files via upload

--- 3.2.js
+++ 3.2.js
@@ -1,21 +1,69 @@
-const intify = _ => parseInt(_, 10),
-   sum = _ => _.reduce((a, i) => a + i, 0),
-   sortNums = (a, b) => a < b ? -1 : 1;
+/*
+PART 2
 
+As you finish identifying the misplaced items, the Elves come to you with another issue.
 
-const o = [];
+For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For 
+efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, 
+if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at 
+most two of the Elves will be carrying any other item type.
 
-const i = (require('fs')).readFileSync('./inputs/3.txt', 'utf-8').trim()
-   .split('\n')
+The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges 
+need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item 
+type is the right one is by finding the one item type that is common between all three Elves in each group.
+Every set of three lines in your list corresponds to a single group, but each group can have a different badge item 
+type. So, in the above example, the first group's rucksacks are the first three lines:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+
+And the second group's rucksacks are the next three lines:
+
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. 
+In the second group, their badge item type must be Z.
+
+Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for 
+the first group and 52 (Z) for the second group. The sum of these is 70.
+
+Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those 
+item types?
+
+*/
+
+const sum = _ => _.reduce((a, i) => a + i, 0);
+
+// i2 will store the groups of 3 as arrays, we can push into this
+// based on the original index of Math.floor(index / 3) (or ~~)
+const i2 = [];
+const i = (require('fs')).readFileSync('./inputs/3.txt', 'utf-8').trim().split('\n')
    .forEach((_, i) => {
-      const oi = ~~(i / 3);
-      if (!o[oi]) {
-         o[oi] = [];
-      }
-      o[oi].push(_);
+      // i2i is the index of i2 that we want to add to
+      const i2i = ~~(i / 3);
+      // if i2[i2i] hasn't been filled in yet, initialize it with the value, 
+      // otherwise, push it on
+      // could be written more verbosely, but this is nice an unreadable :)
+      //
+      // if (i2[i2i] === undefined) {
+      //    i2[i2i] = [];
+      // }
+      // i2[i2i].push(_);
+      //
+      i2[i2i] = !i2[i2i] ? [_] : [...i2[i2i], _]
    });
 
-const o2 = o.map(_ => {
+
+// Now that we have our arrays split, we need to find the char common
+// in all 3 elements. We can assume that there will be one (and only one) duplicate char
+// so we can optimize be using the shortest array in the list, splice it out of the original
+// and .includes() in the other 2 strings
+const out = i2.map(_ => {
       let s = 0;
       _.forEach((__, i) => s = __.length < _[s].length ? i : s);
       const _2 = _.splice(s, 1)[0];
@@ -26,8 +74,9 @@ const o2 = o.map(_ => {
          }
       }
    })
-   .map(_ => _.charCodeAt(0) - 96 < 0 ? _.charCodeAt(0) - 38 : _.charCodeAt(0) - 96);
+   .map(_ => _.charCodeAt(0) - 96)
+   .map(_ => _ < 0 ? _ + 58 : _);
 
 
-console.log('PART 2', sum(o2));
+console.log('PART 2', sum(out));
 
--- 3.js
+++ 3.js
@@ -1,10 +1,58 @@
-const intify = _ => parseInt(_, 10),
-   sum = _ => _.reduce((a, i) => a + i, 0),
-   sortNums = (a, b) => a < b ? -1 : 1;
+/*
+--- Day 3: Rucksack Reorganization ---
 
-const i = (require('fs')).readFileSync('./inputs/3.txt', 'utf-8').trim()
-   .split('\n')
+One Elf has the important job of loading all of the rucksacks with supplies for the jungle journey. Unfortunately, 
+that Elf didn't quite follow the packing instructions, and so a few items now need to be rearranged.
+
+Each rucksack has two large compartments. All items of a given type are meant to go into exactly one of the two 
+compartments. The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.
+
+The Elves have made a list of all of the items currently in each rucksack (your puzzle input), but they need your 
+help finding the errors. Every item type is identified by a single lowercase or uppercase letter (that is, a and A 
+refer to different types of items).
+
+The list of items for each rucksack is given as characters all on a single line. A given rucksack always has the 
+same number of items in each of its two compartments, so the first half of the characters represent items in the 
+first compartment, while the second half of the characters represent items in the second compartment.
+
+For example, suppose you have the following list of contents from six rucksacks:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+   - The first rucksack contains the items vJrwpWtwJgWrhcsFMMfFFhFp, which means its first compartment contains 
+     the items vJrwpWtwJgWr, while the second compartment contains the items hcsFMMfFFhFp. The only item type that 
+     appears in both compartments is lowercase p.
+   - The second rucksack's compartments contain jqHRNqRjqzjGDLGL and rsFMfFZSrLrFZsSL. The only item type that 
+     appears in both compartments is uppercase L.
+   - The third rucksack's compartments contain PmmdzqPrV and vPwwTWBwg; the only common item type is uppercase P.
+   - The fourth rucksack's compartments only share item type v.
+   - The fifth rucksack's compartments only share item type t.
+   - The sixth rucksack's compartments only share item type s.
+
+To help prioritize item rearrangement, every item type can be converted to a priority:
+
+    Lowercase item types a through z have priorities 1 through 26.
+    Uppercase item types A through Z have priorities 27 through 52.
+
+In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 
+38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
+
+Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of 
+those item types?
+*/
+
+const sum = _ => _.reduce((a, i) => a + i, 0);
+
+const i = (require('fs')).readFileSync('./inputs/3.txt', 'utf-8').trim().split('\n')
    .map(_ => {
+      // I would split the string in half and compare each string,
+      // but this approach of just iterating through seemed algorithmically
+      // faster
       for (let i = 0; i < _.length / 2; i++) {
          for (let j = _.length / 2; j < _.length; j++) {
             if (_[i] === _[j]) {
@@ -13,9 +61,15 @@ const i = (require('fs')).readFileSync('./inputs/3.txt', 'utf-8').trim()
          }
       }
    })
-   .map(_ => _.charCodeAt(0) - 96 < 0 ? _.charCodeAt(0) - 38 : _.charCodeAt(0) - 96);
 
-
+   // Funky way of mapping the numbers:
+   // ASCII Codes are A-Z[\]^_a-z, A = 65, a = 97
+   // if we subtract 96, we get a-z mapped to the right values, 
+   // leaving A-Z in the range of -31 - -5
+   // so we just check if the value is < 0, add 58 (31 + 27) to bring us to
+   // 27 - 52!
+   .map(_ => _.charCodeAt(0) - 96)
+   .map(_ => _ < 0 ? _ + 58 : _);
 
 console.log('PART 1', sum(i));
 
